91adb4a15b049ec87c10e0153350cd46816423cf
Evan Hildreth <me@eph.me>
2022-09-16 17:45:59 -0400

    Use DI and events

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 namespace Smolblog\Twitter;
 
 use Smolblog\Core\App;
+use Smolblog\Core\Events\CollectingConnectors;
 use Smolblog\Core\Factories\ConnectionCredentialFactory;
 use Smolblog\Core\Registrars\ConnectorRegistrar;
 use Smolblog\OAuth2\Client\Provider\Twitter;
@@ -15,14 +16,15 @@ use Smolblog\OAuth2\Client\Provider\Twitter;
  */
 function loadPlugin(App $smolblog) {
 	// Create the Twitter connector and register it.
-	$twitter_connector = new TwitterConnector(
-		provider: new Twitter(
-			array(
-				'clientId'     => 'mememe',
-				'clientSecret' => 'youyou',
-			)
-		),
-		factory: $smolblog->container->get(ConnectionCredentialFactory::class)
-	);
-	$smolblog->container->get(ConnectorRegistrar::class)->register($twitter_connector);
+	$smolblog->container->addShared(Twitter::class, fn() => new Twitter([
+		'clientId' => 'mememe',
+		'clientSecret' => 'youyou'
+	]));
+	$smolblog->container->addShared(TwitterConnector::class)
+		->addArgument(Twitter::class)
+		->addArgument(ConnectionCredentialFactory::class);
+
+	$smolblog->events->subscribeTo(CollectingConnectors::class, function ($event) {
+		$event->connectors[] = TwitterConnector::class;
+	});
 }
