c8d772be60744f2c4ec5c06195c82f1711ab21e5
Evan Hildreth <me@eph.me>
2022-09-15 21:00:53 -0400

    Fix the namespacing

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 namespace Smolblog\Twitter;
 
 use Smolblog\Core\App;
+use Smolblog\Core\Factories\ConnectionCredentialFactory;
+use Smolblog\Core\Registrars\ConnectorRegistrar;
+use Smolblog\OAuth2\Client\Provider\Twitter;
 
 /**
  * Load the plugin's classes into the App's container and subscribe to any events.
@@ -13,7 +16,7 @@ use Smolblog\Core\App;
 function loadPlugin(App $smolblog) {
 	// Create the Twitter connector and register it.
 	$twitter_connector = new TwitterConnector(
-		provider: new TwitterOAuth(
+		provider: new Twitter(
 			array(
 				'clientId'     => 'mememe',
 				'clientSecret' => 'youyou',
